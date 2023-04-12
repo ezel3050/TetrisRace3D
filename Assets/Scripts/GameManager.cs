a98e3fe326a372f1e26b365cd8a7c6a8f963b050
Inex <31888517+ezel3050@users.noreply.github.com>
2023-04-12 18:45:24 +0430

    d

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        InputManager.movementInputActive = false;
+      //  InputManager.movementInputActive = false;
         ActivateCurrentLevel();
         InitilizeLevel();
     }
